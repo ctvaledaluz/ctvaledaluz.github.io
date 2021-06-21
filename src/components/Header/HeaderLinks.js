b42d8f10c3b88ecf950fd88974268a3f5c6322e1
Jean Patrick Wilhvock <je4n.pw@gmail.com>
2021-06-21 11:48:29 -0300

    Alterações no layout e ajustes textuais

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -8,20 +8,29 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import Close from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
 
 // @material-ui/icons
 import { Apps, CloudDownload } from "@material-ui/icons";
 
 // core components
-import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
 import Button from "components/CustomButtons/Button.js";
 
 import styles from "assets/jss/site-styles/components/headerLinksStyle.js";
 
 const useStyles = makeStyles(styles);
-
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
 export default function HeaderLinks(props) {
   const classes = useStyles();
+  const [classicModal, setClassicModal] = React.useState(false);
   return (
     <List className={classes.list}>
       <ListItem className={classes.listItem}>
@@ -31,7 +40,7 @@ export default function HeaderLinks(props) {
           classes={{ tooltip: classes.tooltip }}
         >
           <Link to="/" className={classes.navLink}>
-            <Button color="transparent" size="small" style={{margin: 0,  padding: 0}}>
+            <Button color="transparent" size="sm" style={{margin: 0,  padding: 0}}>
               <i className={classes.socialIcons + " fa fa-home"} /> Página Principal
             </Button>
           </Link>
@@ -44,10 +53,25 @@ export default function HeaderLinks(props) {
           classes={{ tooltip: classes.tooltip }}
         >
           <Link to="/history" className={classes.navLink}>
-            <Button color="transparent" size="small" style={{margin: 0,  padding: 0}}>
+            <Button color="transparent" size="sm" style={{margin: 0,  padding: 0}}>
               <i className={classes.socialIcons + " fa fa-book"} /> Sobre
             </Button>
           </Link>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          title="Como Ajudar"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button color="transparent"
+                  size="sm"
+                  className={classes.navLink}
+                  onClick={() => setClassicModal(true)}
+          >
+            <i className={classes.socialIcons + " fa fa-hands-helping"} /> Como Ajudar
+          </Button>
         </Tooltip>
       </ListItem>
       <ListItem className={classes.listItem}>
@@ -62,6 +86,53 @@ export default function HeaderLinks(props) {
             color="transparent"
             className={classes.navLink}
           >
+            <Dialog
+              classes={{
+                root: classes.center,
+                paper: classes.modal,
+              }}
+              open={classicModal}
+              TransitionComponent={Transition}
+              keepMounted
+              onClose={() => setClassicModal(false)}
+              aria-labelledby="classic-modal-slide-title"
+              aria-describedby="classic-modal-slide-description"
+            >
+              <DialogTitle
+                id="classic-modal-slide-title"
+                disableTypography
+                className={classes.modalHeader}
+              >
+                <IconButton
+                  className={classes.modalCloseButton}
+                  key="close"
+                  aria-label="Close"
+                  color="inherit"
+                  onClick={() => setClassicModal(false)}
+                >
+                  <Close className={classes.modalClose} />
+                </IconButton>
+                <h4 className={classes.modalTitle}>Você deseja contribuir?</h4>
+              </DialogTitle>
+              <DialogContent
+                id="classic-modal-slide-description"
+                className={classes.modalBody}
+              >
+                <p>Banco do Brasil</p>
+                <p>Agencia 5214-0</p>
+                <p>Conta Corrente - 7693-7</p>
+                <p>CNPJ 81.140.360/0001-90</p>
+              </DialogContent>
+              <DialogActions className={classes.modalFooter}>
+                <Button
+                  onClick={() => setClassicModal(false)}
+                  color="info"
+                  simple
+                >
+                  Fechar
+                </Button>
+              </DialogActions>
+            </Dialog>
             <i className={classes.socialIcons + " fa fa-map-marker"} /> Localização
           </Button>
         </Tooltip>
